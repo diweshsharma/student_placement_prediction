2c28b0c5ce4f7094d4777d7f233eabbab4de9f13
diweshsharma <diweshsharma.janu@gmail.com>
2026-06-09 21:05:42 +0530

    made chnages and addclassifier model

---
+++ src/preprocessing.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler , OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
+categorical ={
+    'branch',
+    'college_tier'
+}
+
+numerical ={
+    "cgpa",
+    "backlogs",
+    "coding_skills",
+    "dsa_score",
+    "aptitude_score",
+    "communication_skills",
+    "ml_knowledge",
+    "system_design",
+    "internships",
+    "projects_count",
+    "certifications",
+    "hackathons",
+    "open_source_contributions",
+    "extracurriculars"                
+
+}
+
+def preprocess():
+    return ColumnTransformer(
+        transformers=[(
+            'encoder',
+            OneHotEncoder(handle_unknown = 'ignore' ),
+            categorical
+            
+        ),
+        ('scaler',
+         StandardScaler(),
+         numerical
+            
+        )              
+            
+        ]
+        
+    )
+    
+    
